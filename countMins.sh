26e8ab9e5084618eab1472ec933b607ca850905c
Nathan Hartland <hartland@hydra.(none)>
2015-10-27 15:15:35 +0000

    histogram scripts

--- countMins.sh
+++ countMins.sh
@@ -1,1 +1,1 @@
-tail *-$1.sh.o* -n 2 -q | awk 'NR % 2 {print 60*$5+$7+$9/60}' | ./histogram.py --max=100 --min=0
+tail *-$1.sh.o* -n 2 -q | awk 'NR % 2 {print 60*$5+$7+$9/60}' | ./histogram.py --max=700 --min=0 -p -b20
