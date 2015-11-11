3791aafe434fed295109e9264deb10d4ab227b56
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2015-11-11 11:41:52 +0000

    new POS

---
+++ genFK_scripts/POSDYCBS_genFK.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -e "FK_POSDYCBS.dat" ] 
+then
+        exit 0;
+else
+        exit 1;
+fi 
---
+++ genFK_scripts/POSDYCD_genFK.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -e "FK_POSDYCD.dat" ] 
+then
+        exit 0;
+else
+        exit 1;
+fi 
