be49835e8bd6c1c8475ddf118f1ebd86f9924d3a
Nathan Hartland <hartland@hydra.(none)>
2016-01-26 15:55:19 +0000

    improved hydra applgrid script

--- scripts/suball_HY_APP.sh
+++ scripts/suball_HY_APP.sh
@@ -4,7 +4,7 @@ TH=$1
 for i in `seq 0 76`;
 do
     FILENAME=apfelcomb$i-$TH
-    echo "../appl_comb $i $TH
+    echo "./appl_comb $i $TH
 rc=\$?; if [[ \$rc != 0 ]]; then echo \$rc > ./err/$FILENAME; fi" > $FILENAME
     chmod +x $FILENAME
     echo $FILENAME
