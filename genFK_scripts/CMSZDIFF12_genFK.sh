e5564d2fa74885cb223f6c68b3dde2d80d2a7aba
Alberto Guffanti <alberto.guffanti@nbi.dk>
2016-05-25 16:54:46 +0200

    Added genFK script and fixed applgrid.db entry for CMSZDIFF12.

---
+++ genFK_scripts/CMSZDIFF12_genFK.sh
@@ -0,0 +1,10 @@
+# Run merge
+FKmerge FK_CMSZDIFF12-BIN1.dat FK_CMSZDIFF12-BIN2.dat FK_CMSZDIFF12-BIN3.dat FK_CMSZDIFF12-BIN4.dat FK_CMSZDIFF12-BIN5.dat > FK_CMSZDIFF12.dat 
+
+# Check return value
+RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
+
+# Cleanup 
+rm FK_CMSZDIFF12-BIN*
+
+exit 0
