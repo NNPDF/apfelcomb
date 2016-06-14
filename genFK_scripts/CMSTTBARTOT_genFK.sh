82e3505317124382004c9cc67294d41328020613
Emanuele Nocera <noceral@tplxlap211.nat.physics.ox.ac.uk>
2016-06-14 20:41:38 +0100

    Fixed issue in ATLAS and CMS TTBARTOT genFK scripts

--- genFK_scripts/CMSTTBARTOT_genFK.sh
+++ genFK_scripts/CMSTTBARTOT_genFK.sh
@@ -1,10 +1,10 @@
 # merge FK tables
-FKmerge FK_CMSTTBARTOT7TEV.dat CMSTTBARTOT8TEV.dat > FK_CMSTTBARTOT.dat
+FKmerge FK_CMSTTBARTOT7TEV.dat FK_CMSTTBARTOT8TEV.dat > FK_CMSTTBARTOT.dat
 
 # Check return value
 RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
 
 # Cleanup
-rm FK_CMSTTBARTOT7TEV.dat CMSTTBARTOT8TEV.dat
+rm FK_CMSTTBARTOT7TEV.dat FK_CMSTTBARTOT8TEV.dat 
 
 exit 0
