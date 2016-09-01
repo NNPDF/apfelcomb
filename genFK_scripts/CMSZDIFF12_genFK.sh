6b591073e9577b7a54cf64f14f224fcf7327a701
Maria Ubiali <ubiali@pcjy.hep.phy.cam.ac.uk>
2016-09-01 10:24:05 +0100

    Fixed CMS script

--- genFK_scripts/CMSZDIFF12_genFK.sh
+++ genFK_scripts/CMSZDIFF12_genFK.sh
@@ -1,10 +1,7 @@
 # Run merge
-FKmerge FK_CMSZDIFF12-BIN1.tmp.dat FK_CMSZDIFF12-BIN2.tmp.dat FK_CMSZDIFF12-BIN3.tmp.dat FK_CMSZDIFF12-BIN4.tmp.dat FK_CMSZDIFF12-BIN5.tmp.dat > FK_CMSZDIFF12.dat 
+FKmerge FK_CMSZDIFF12-BIN1.dat FK_CMSZDIFF12-BIN2.dat FK_CMSZDIFF12-BIN3.dat FK_CMSZDIFF12-BIN4.dat FK_CMSZDIFF12-BIN5.dat > FK_CMSZDIFF12.dat 
 
 # Check return value
 RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
-
-# Cleanup 
-rm FK_CMSZDIFF12-BIN*.tmp.dat
 
 exit 0
