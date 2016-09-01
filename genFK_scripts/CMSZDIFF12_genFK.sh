545331f8304967c07e2dfaac516aad6e4c334953
Maria Ubiali <ubiali@pcjy.hep.phy.cam.ac.uk>
2016-09-01 10:08:04 +0100

    Modified CMS gen script

--- genFK_scripts/CMSZDIFF12_genFK.sh
+++ genFK_scripts/CMSZDIFF12_genFK.sh
@@ -1,16 +1,10 @@
 # Run merge
-FKincrement FK_CMSZDIFF12-BIN1.dat 2 > FK_CMSZDIFF12-BIN1.tmp.dat
-FKincrement FK_CMSZDIFF12-BIN2.dat 2 > FK_CMSZDIFF12-BIN2.tmp.dat
-FKincrement FK_CMSZDIFF12-BIN3.dat 2 > FK_CMSZDIFF12-BIN3.tmp.dat
-FKincrement FK_CMSZDIFF12-BIN4.dat 2 > FK_CMSZDIFF12-BIN4.tmp.dat
-FKincrement FK_CMSZDIFF12-BIN5.dat 2 > FK_CMSZDIFF12-BIN5.tmp.dat
-
 FKmerge FK_CMSZDIFF12-BIN1.tmp.dat FK_CMSZDIFF12-BIN2.tmp.dat FK_CMSZDIFF12-BIN3.tmp.dat FK_CMSZDIFF12-BIN4.tmp.dat FK_CMSZDIFF12-BIN5.tmp.dat > FK_CMSZDIFF12.dat 
 
 # Check return value
 RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
 
 # Cleanup 
-rm FK_CMSZDIFF12-BIN*
+rm FK_CMSZDIFF12-BIN*.tmp.dat
 
 exit 0
