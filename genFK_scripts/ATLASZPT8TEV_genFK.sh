27e58dc0203ec3060a7a9a38fa0ad52d35c1cf68
Maria Ubiali <ubiali@pcjy.hep.phy.cam.ac.uk>
2016-10-03 16:01:34 +0100

    Fixed ATLASZPT8TEV_genFK.sh script

--- genFK_scripts/ATLASZPT8TEV_genFK.sh
+++ genFK_scripts/ATLASZPT8TEV_genFK.sh
@@ -2,8 +2,14 @@
 FKincrement FK_ATLASZPT8TEV-MLLBIN4_ptZ.dat 10 > FK_ATLASZPT8TEV-MLLBIN4_ptZ.tmp.dat
 FKincrement FK_ATLASZPT8TEV-MLLBIN6_ptZ.dat 10 > FK_ATLASZPT8TEV-MLLBIN6_ptZ.tmp.dat
 
-## ONLy mll distributions so far - exclude bin 5 so not to overlap with YDIST!
+## Exclude bin 5 from MLL distribution so that it does not overlap with YDIST!
+## Unnormalised distributions
 FKmerge FK_ATLASZPT8TEV-MLLBIN1_ptZ.dat FK_ATLASZPT8TEV-MLLBIN2_ptZ.dat FK_ATLASZPT8TEV-MLLBIN3_ptZ.dat FK_ATLASZPT8TEV-MLLBIN4_ptZ.tmp.dat FK_ATLASZPT8TEV-MLLBIN6_ptZ.tmp.dat > FK_ATLASZPT8TEVMDIST.dat
+
+## Merge 6 bins of YLL distribution so that it does not overlap with YDIST!
+## Unnormalized distributions
+FKmerge FK_ATLASZPT8TEV-YLLBIN1_ptZ.dat FK_ATLASZPT8TEV-YLLBIN2_ptZ.dat FK_ATLASZPT8TEV-YLLBIN3_ptZ.dat FK_ATLASZPT8TEV-YLLBIN4_ptZ.dat FK_ATLASZPT8TEV-YLLBIN5_ptZ.dat FK_ATLASZPT8TEV-YLLBIN6_ptZ.dat > FK_ATLASZPT8TEVYDIST.dat
+
 RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
 
 # Cleanup
