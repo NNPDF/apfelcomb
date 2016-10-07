f92fba4089790f831a3065002c0c3a84082e02af
Nathan Hartland <hartland@nnpdf.(none)>
2016-10-07 07:50:37 +0100

    fix CMSWMU8TEV genFK filenames

--- genFK_scripts/CMSWMU8TEV_genFK.sh
+++ genFK_scripts/CMSWMU8TEV_genFK.sh
@@ -1,10 +1,10 @@
 # merge FK tables
-FKmerge FK_CMSWMU8TEV-WP_leptrap.dat FK_CMSWMU8TEV-WM_leptrap.dat > FK_CMSWMU8TEV.dat
+FKmerge FK_CMSWMU8TEV_WP_leptrap.dat FK_CMSWMU8TEV_WM_leptrap.dat > FK_CMSWMU8TEV.dat
 
 # Check return value
 RETCODE=$?; if [[ $RETCODE != 0 ]]; then exit $RETCODE; fi
 
 # Cleanup 
-rm FK_CMSWMU8TEV-WP_leptrap.dat FK_CMSWMU8TEV-WM_leptrap.dat
+rm FK_CMSWMU8TEV_WP_leptrap.dat FK_CMSWMU8TEV_WM_leptrap.dat
 
 exit 0
