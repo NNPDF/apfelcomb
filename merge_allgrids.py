c28ea357bb200d6b220bcca1abc91b53ff2fc859
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2017-03-12 17:29:41 +0100

    test merge tools

--- merge_allgrids.py
+++ merge_allgrids.py
@@ -5,13 +5,12 @@ import sys,os
 from subprocess import call
 
 def infoSplash():
-    print 'Usage: ' + sys.argv[0] + " <gridID> <theoryID>"
+    print 'Usage: ' + sys.argv[0] + " <theoryID>"
     exit(-1)
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     infoSplash()
-gridID = sys.argv[1]
-theoryID = sys.argv[2]
+theoryID = sys.argv[1]
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
 sqlite3Path = currentPath+'/db/applgrid.db'
@@ -23,14 +22,13 @@ try:
     con = lite.connect(sqlite3Path)
     cur = con.cursor()    
     
-    cur.execute('SELECT name FROM grids WHERE id = \''+gridID+'\' ORDER BY id ' )
+    cur.execute('SELECT name FROM grids ORDER BY id' )
     grids = cur.fetchall()
-    if len(grids) != 1:
-        print "RowError: Either cannot find id: " + gridID + " or something very strange has happened"
+    if len(grids) == 1:
+        print "Error: no grids found!"
         exit(-1)
-
-    # merge the grids
-    mergegrid(grids[0][0], theoryID, cur)
+    for grid in grids:
+        mergegrid(grid[0], theoryID, cur)
    
     
 except lite.Error, e:
---
+++ mergecommon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sqlite3 as lite
+import sys,os
+from subprocess import call
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def mergegrid(target, thID, cur):
+    print "Processing grid " + bcolors.HEADER + target + bcolors.ENDC+ ", theory " + bcolors.HEADER+ str(thID) + bcolors.ENDC
+    # Search for subgrids
+    currentPath = os.path.dirname(os.path.realpath(__file__))
+    subgridPath = currentPath+'/results/theory_'+str(thID)+'/subgrids/'
+    ftargetPath = currentPath+'/results/theory_'+str(thID)+'/fastkernel/'
+    cur.execute('SELECT id FROM subgrids WHERE fktarget = \''+target+'\' ' )
+    subgrids = cur.fetchall()
+    if len(subgrids) <= 0:
+        print "Cannot find any subgrids for target: " + target
+        exit(-1)
+
+    mergeTarget = ftargetPath+"FK_"+target+".dat"
+    mergeConstituents = ''
+    complete_subgrids = True
+    for subgrid in subgrids:
+        constituent = subgridPath+"FK_"+target+"_"+str(subgrid[0])+".subgrid.dat"
+        if os.path.isfile(constituent) != True: 
+            print " -   Missing subgrid: " + str(subgrid[0])
+            complete_subgrids = False
+        mergeConstituents = mergeConstituents + constituent + ' '
+
+    if complete_subgrids == True:
+        os.system("FKmerge2 " + mergeConstituents+' > '+ mergeTarget)  
+        print target + bcolors.OKGREEN + " successfully generated!" + bcolors.ENDC
+    else:
+        print target + bcolors.FAIL + " generation failed!" + bcolors.ENDC
+
+    # print mergeConstituents
