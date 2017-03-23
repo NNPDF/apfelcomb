80b2c2e6de246b0e084706e2c1f0799c9bce523d
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2017-03-23 12:37:17 +0100

    runallCfac

--- runallCfac.sh
+++ runallCfac.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-dataDir="../nnpdfcpp/data"
-cFactors=$dataDir"/NNLOCFAC/registeredCFactors.dat"
+dataDir="../nnpdfcpp/data/"
+sourceDir=$dataDir"NNLOCFAC/"
+cFactors=$sourceDir"registeredCFactors.dat"
 
 echo "Processing theoryID: " $1
 
 while read p; do
   a=( $p )
-  echo ${a[0]} ${a[1]}
-  ./src/cfac_scale $1 ${a[0]} ${a[1]}
+  if [ "${a[0]}" = "QCD" ]; then
+  	echo ${a[0]} ${a[1]} ${a[2]} ${a[3]}
+  	./src/cfac_scale $1 ${a[1]} ${a[2]} ${a[3]}
+  else
+  	echo cp $sourceDir"CF_"${a[0]}"_"${a[1]}".dat" $dataDir"theory_"$1"/cfactor/"
+  	cp $sourceDir"CF_"${a[0]}"_"${a[1]}".dat" $dataDir"theory_"$1"/cfactor/"
+  fi
 done < $cFactors
