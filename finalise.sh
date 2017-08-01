5cac789bbffd7a6e0b54fbace4cef573f650ee74
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2017-08-01 15:02:54 +0200

    new finalise script

--- finalise.sh
+++ finalise.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This script finalises a theory
 
-echo "Processing theoryID: " $1
-./merge_allgrids.py $1
+echo "Finalising theoryID: " $1
 ./run_cfactors.py $1
 cp -r ./compound ./results/theory_$1/
+rm -rf ./results/theory_$1/subgrids/
+cd results
+tar -cvzf ./theory_$1.tgz ./theory_$1/
+cd ../
