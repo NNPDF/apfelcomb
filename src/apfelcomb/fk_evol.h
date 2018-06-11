2d06a3c65a59fb08294ef26eacbaf44c67901bc4
Nathan Hartland <n.p.hartland@vu.nl>
2018-06-11 13:43:28 +0200

    Evol kinematics (hopefully) finished

--- src/apfelcomb/fk_evol.h
+++ src/apfelcomb/fk_evol.h
@@ -1,9 +1,4 @@
 #pragma once
-/*
- *  fk_dis.h
- *  APFEL DIS conversion to FK
- * *  nph 09/14
- */
 
 #include <stdlib.h>
 #include <fstream>
@@ -32,34 +27,17 @@ namespace EVL
                        vector<double> const& q2grid );
 
     };
-//	// A DIS SubGrid essentially just aliases the parent FKSubGrid's CommonData attribute
-//	// I put this into a 'SubGrid' such that we can use the same testing/optimisation procedures as in APPLgrids
-//	class SubGrid: public FKSubGrid
-//	{
-//	public:
-//	void Compute(QCD::qcd_param const&, vector<double>&) const;			//!< Compute APPLgrid results mapped to Commondata
-//	void Combine(QCD::qcd_param const&, NNPDF::FKGenerator*) const;		//!< Perform the FK combination on a subgrid
-//	private:
-//		friend class ::FKTarget;
-//		SubGrid(FKTarget const& parent, NNPDF::IndexDB const& db, int const& iDB):
-//		FKSubGrid(parent, iDB, NNPDF::dbquery<string>(db, iDB, "operators")),
-//	    process(NNPDF::dbquery<string>(db,iDB,"process"))
-//		{
-//			if (incdat != 0 || muldat != 1)
-//			{
-//				cerr << "Error: DIS grids do not support operators other than normalisation" << endl;
-//				exit(-1);
-//			}
-//		};
-//
-//		void Splash(ostream&) 	const;	//!< Print metadata to stream
-//		size_t GetNdat() 		const;	//!< Return number of datapoints in subgrid
-//		double GetQ2max() 		const;	//!< Return maximum scale used in a subgrid
-//		double GetXmin() 		const;	//!< Return minimum x-value used in this sub grid
-//
-//		// **********************************************************
-//
-//		const string process;			//!< Process string of the observable
-//	};
+
+    // Generate output (evolved scale) x-grid
+    vector<double> generate_xgrid( const int nx,
+            const double xmin,
+            const double xmed,
+            const double xmax);
+
+    // Generate output q2 subgrids
+    // One subgrid is generated for every HQ threshold crossed
+    vector< vector<double> > generate_q2subgrids( QCD::qcd_param par, const int nq2,
+            const double q2min, const double q2max);
+
 }
 
--- src/core/fk_evol.cc
+++ src/core/fk_evol.cc
@@ -1,4 +1,5 @@
 #include "apfelcomb/fk_evol.h"
+#include <array>
 
 
 namespace EVL
@@ -30,5 +31,81 @@ namespace EVL
                 }
     }
 
+    // allocate grid in x
+    vector<double> generate_xgrid( const int nx,
+            const double xmin,
+            const double xmed,
+            const double xmax)
+    {
+        vector<double> xg;
+        const int nxm = nx/2;
+        for (int ix = 1; ix <= nx; ix++)
+        {
+            if (ix <= nxm)
+                xg.push_back(xmin*pow(xmed/xmin,2.0*(ix-1.0)/(nx-1.0)));
+            else
+                xg.push_back(xmed+(xmax-xmed)*((ix-nxm-1.0)/(nx-nxm-1.0)));
+        }
+        return xg;
+    };
+
+    // Generates a list of nq2 Q2 points between q2min and q2max.
+    // Distributed as linear in tau = log( log( Q2 / lambda2 ) )
+    vector<double> generate_q2grid( const int nq2,
+            const double q2min,
+            const double q2max,
+            const double lambda2
+            )
+    {
+        vector<double> q2grid;
+        const double tau_min   = log( log( q2min / lambda2 ));
+        const double tau_max   = log( log( q2max / lambda2 ));
+        const double delta_tau = (tau_max - tau_min) /( (double) nq2 - 1);
+
+        for (int iq2 = 0; iq2 < nq2; iq2++)
+            q2grid.push_back(lambda2 * exp(exp( tau_min + iq2*delta_tau)));
+        return q2grid;
+    }
+
+    // Compute Q2 grid
+    // This is suitable for PDFs not AlphaS (see maxNF used below)
+    vector< vector<double> > generate_q2subgrids( QCD::qcd_param par, const int nq2,
+            const double q2min, const double q2max)
+    {
+        const double eps     = 1E-4;
+        const double lambda2 = 0.0625e0;
+        const int nfpdf = atoi(par.thMap["MaxNfPdf"].c_str());
+
+        const std::array<double, 3> hqth = {
+            pow(atof(par.thMap["Qmc"].c_str())*atof(par.thMap["kcThr"].c_str()),2),
+            pow(atof(par.thMap["Qmb"].c_str())*atof(par.thMap["kbThr"].c_str()),2),
+            pow(atof(par.thMap["Qmt"].c_str())*atof(par.thMap["ktThr"].c_str()),2),
+        };
+
+        // Determine subgrid edges
+        vector<double> subgrid_edges={q2min};
+        for (int i=0; i<(nfpdf-3); i++)
+            if (hqth[i] > q2min) subgrid_edges.push_back(hqth[i]);
+        subgrid_edges.push_back(q2max);
+
+        // Determine point distribution across subgrids and generate subgrids
+        const std::vector< double > point_dist = generate_q2grid(nq2, q2min, q2max, lambda2);
+        std::vector< vector< double > > q2grid_subgrids;
+        for (int i=0; i<subgrid_edges.size()-1; i++)
+        {
+            const double min_edge = subgrid_edges[i];
+            const double max_edge = subgrid_edges[i+1];
+            auto in_sub = [=](double q2pt){return (q2pt - min_edge) > -eps && (q2pt - max_edge) < eps;};
+            const int npoints_sub = std::count_if(point_dist.begin(), point_dist.end(), in_sub);
+
+            if (npoints_sub < 2)
+                throw std::runtime_error("Too few points to sufficiently populate subgrids. More Q points required");
+
+            const vector< double > subgrid = generate_q2grid(npoints_sub, min_edge, max_edge, lambda2);
+            q2grid_subgrids.push_back(subgrid);
+        }
+
+        return q2grid_subgrids;
+    }
 }
 
