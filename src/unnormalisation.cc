0eb2d87b69eada53824fb5c1a09ead1d37e9eed9
Rabah Abdul Khalek <rabah.khalek@gmail.com>
2018-10-11 14:40:25 +0200

    update to the unnormalization code of F2D

--- src/unnormalisation.cc
+++ src/unnormalisation.cc
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <cstdio>
+#include <limits>
 
 #include "apfelcomb/fk_dis.h"
 #include "apfelcomb/fk_utils.h"
@@ -18,20 +19,11 @@
 
 using namespace std;
 
-/*
-double getQ2Max_CommonData(NNPDF::CommonData const& cd)
-{
-double q2max = 0;
-for (int i=0; i<cd.GetNData(); i++)
-q2max = fmax(q2max, cd.GetKinematics(i,1));
-return q2max;
-}*/
-
 int main(int argc, char* argv[]) {
 
-  if ( argc != 5 )
+  if ( argc != 6 )
   {
-    std::cerr << "Usage: " <<argv[0] <<"<source=app/dis/dyp> <database id> <theory id> <set_name>" <<std::endl;
+    std::cerr << "Usage: " <<argv[0] <<"<source=app/dis/dyp> <database id> <theory id> <set_name> <proton_baseline>" <<std::endl;
     exit(-1);
   }
 
@@ -40,7 +32,8 @@ int main(int argc, char* argv[]) {
   const string source = argv[1];
   const int iDB = atoi(argv[2]);
   const int iTh = atoi(argv[3]);
-  const std::string setName = argv[4];
+  const string setName = argv[4];
+  const string pdfset=argv[5];
   setupDir(iTh);
 
   const std::string cDataPath = dataPath() + "/commondata/DATA_"+setName+".dat";
@@ -66,9 +59,9 @@ int main(int argc, char* argv[]) {
   const std::vector<int> grid_matches = NNPDF::dbmatch(grid_db, "name", fktarget);
 
   if ( fktarget == "" ) // A bit clumsy, doing better would require modifying dbquery to throw an error when no match is found
-    throw std::runtime_error("Cannot find subgrid "+to_string(iDB)+" in apfelcomb database");
+  throw std::runtime_error("Cannot find subgrid "+to_string(iDB)+" in apfelcomb database");
   if ( grid_matches.size() == 0 )
-    throw std::runtime_error("Cannot find FK target "+fktarget+" in apfelcomb database");
+  throw std::runtime_error("Cannot find FK target "+fktarget+" in apfelcomb database");
 
   const int target = grid_matches[0];
   FKTarget table(grid_db, target, par.global_nx); table.ReadSubGrids(subgrid_db);
@@ -79,30 +72,13 @@ int main(int argc, char* argv[]) {
 
   double x,Q,Q2,y;
 
-  /*
-  //Check kinematics
-  cout<<"double x[]={";
-  for (int i=0; i<cd.GetNData(); i++)
-  {    x=cd.GetKinematics(i,0);
-  cout<<x<<", ";
-  }
-  cout<<"};"<<endl;
-
-  cout<<"double Q[]={";
-  for (int i=0; i<cd.GetNData(); i++)
-  {
-    Q2=cd.GetKinematics(i,1);
-    Q=sqrt(Q2);
-    cout<<Q<<", ";
-  }
-  cout<<"};"<<endl;*/
-
+  string command1 = "mkdir "+dataPath() + "/theory_" + to_string(iTh) + "/F2D_unnormalization/"+setName;
+  system(command1.c_str());
 
   int Nrep=100;//100
-  const string pdfset="NNPDF31_nnlo_as_0118";
-  for(int nrep=0; nrep<=Nrep; nrep++)
+  for(int nrep=0; nrep<1; nrep++)
   {
-    const std::string F2DPath = dataPath() + "/theory_" + to_string(tTh) + "/F2D_unnormalization/F2D_"+setName+"_"+to_string(nrep)+".dat";
+    std::string F2DPath = dataPath() + "/theory_" + to_string(iTh) + "/F2D_unnormalization/"+setName+"/F2D_"+setName+"_"+to_string(nrep)+".dat";
     std::ofstream outstream(F2DPath.c_str());
     outstream<<"*******************************************************************************************"<<endl;
     outstream<<"SetName: "<<setName<<endl;
@@ -115,20 +91,62 @@ int main(int argc, char* argv[]) {
     outstream<<"Replica (LHAPDF): "<<nrep<<endl;
     outstream<<"Description: Unnormalization corrections for F2_A/F2_D nuclear ratios"<<endl;
     outstream<<"*******************************************************************************************"<<endl;
+
+    bool FOR_APFELGRID=0;
+
+    if(FOR_APFELGRID)
+    outstream<<endl;
+
+
+
     for (int ndat=0; ndat<cd.GetNData(); ndat++)
     {
-      x=cd.GetKinematics(ndat,0);
-      Q2=cd.GetKinematics(ndat,1);
-      Q=sqrt(Q2);
-      y=cd.GetKinematics(ndat,2);
 
-      QCD::initPDF(pdfset+".LHgrid",nrep);
-      F2_D = QCD::disobs("DIS_F2D",x,Q,y);//Deuterium structure function
-      outstream<<1./F2_D<<"\t"<<0<<endl;
+      if(setName == "nEMCC" || setName == "nEMCCA" || setName == "nEMCFE")
+      {
+        if(FOR_APFELGRID)
+        outstream<<1.<<endl;//" "<<0<<endl;
+        else
+        outstream<<1.<<" "<<0<<endl;
+      }
+      else
+      {
+        x=cd.GetKinematics(ndat,0);
+        Q2=cd.GetKinematics(ndat,1);
+        Q=sqrt(Q2);
+        y=cd.GetKinematics(ndat,2);
+
+        QCD::initPDF(pdfset+".LHgrid",nrep);
+        string obs;
+        //if(setName== nE139AGD nE139ALD nE139AUD nE139BED nE139CAD nE139CD  nE139FED nE139HED
+        //if(setName.find("nE139"))
+        if(setName == "nE139AGD" || setName ==  "nE139ALD" || setName == "nE139AUD" ||
+           setName == "nE139BED" || setName ==  "nE139CAD" || setName == "nE139CD"  ||
+           setName == "nE139FED" || setName == "nE139HED")
+        F2_D = QCD::disobs("DIS_NCE_D",x,Q,y);//Deuterium Cross section
+        else
+        F2_D = QCD::disobs("DIS_F2D",x,Q,y);//Deuterium structure function
+
+        if(F2_D == 0)
+        {
+          if(FOR_APFELGRID)
+          outstream<<std::numeric_limits<double>::signaling_NaN()<<endl;//"\t"<<std::numeric_limits<double>::signaling_NaN()<<endl;
+          else
+          outstream<<std::numeric_limits<double>::signaling_NaN()<<" "<<std::numeric_limits<double>::signaling_NaN()<<endl;
+
+        }
+        else
+        {
+          if(FOR_APFELGRID)
+          outstream<<1./F2_D<<endl;//<<" "<<0<<endl; // or outstream<<2./F2_D<<" "<<0<<endl; ?!
+          else
+          outstream<<1./F2_D<<" "<<0<<endl;
+        }
+      }
     }
-    outstream<<endl;
     outstream.close();
-  }
 
+
+  }
   exit(0);
 }
